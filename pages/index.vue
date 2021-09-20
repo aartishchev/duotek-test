b11456cfae9bb812ddaaba77856078c7a38b25cc
Артищев Алексей <artishev@satel.org>
2021-09-20 20:33:53 +0300

    add initial filter and companies list layout

--- pages/index.vue
+++ pages/index.vue
@@ -18,16 +18,39 @@
 
     <main class="companies__page">
       <h1 class="companies__heading">Каталог компаний</h1>
-      <section>
-        <div class="companies__input-wrapper">
+      <section class="companies__content">
+        <form class="companies__search-form">
           <input
             class="companies__search-input"
             type="search"
             placeholder="Поиск продукта или отрасли"
           />
           <button class="companies__search-button">Найти</button>
-        </div>
+        </form>
+        <ul>
+          <li class="companies__list-item">
+            <h2 class="companies__item-title">Компания</h2>
+            <p class="companies__item-description">Описание</p>
+          </li>
+          <ul>
+            <li class="companies__item-label">Лейбл</li>
+          </ul>
+        </ul>
       </section>
+      <form class="companies__filter">
+        <label class="companies__filter-label" for="branch-select">
+          Отрасль
+        </label>
+        <select id="branch-select" name="branch-filter">
+          <option value="" selected>Все отрасли</option>
+        </select>
+        <label class="companies__filter-label" for="branch-select">
+          Специализация
+        </label>
+        <select id="specialization-select" name="specialization-filter">
+          <option value="" selected>Все специализации</option>
+        </select>
+      </form>
     </main>
   </div>
 </template>
@@ -82,18 +105,26 @@ export default {}
 .companies__page {
   @include centering-layout;
   padding-top: 75px;
+  font-family: var(--font-rubik);
+  display: grid;
+  grid-template-columns: 1fr auto;
+  gap: 18px;
+}
+
+.companies__content {
+  max-width: 745px;
 }
 
 .companies__heading {
+  grid-column: 1 / -1;
   font-family: var(--font-raleway);
   font-size: var(--text-2xl);
   line-height: 1.3;
   font-weight: 600;
   letter-spacing: 0.12px;
-  margin-bottom: 18px;
 }
 
-.companies__input-wrapper {
+.companies__search-form {
   display: flex;
   position: relative;
 
@@ -131,7 +162,6 @@ export default {}
 }
 
 .companies__search-button {
-  font-family: var(--font-rubik);
   font-size: var(--text-small);
   padding: 0 18px;
   background-color: var(--blue-primary);
@@ -143,5 +173,41 @@ export default {}
   &:hover {
     opacity: var(--on-hover-opacity);
   }
+}
+
+.companies__filter {
+  width: 264px;
+  padding: 18px 20px;
+  background-color: var(--gray-247);
+  border-radius: var(--radius-primary);
+}
+
+.companies__filter-label {
+}
+
+.companies__list-item {
+  padding: 0 52px 0 0;
+}
+
+.companies__item-title {
+  font-size: var(--text-xl);
+  line-height: 1;
+  font-weight: normal;
+  color: var(--blue-primary);
+}
+
+.companies__item-description {
+  font-size: var(--text-small);
+  line-height: 1.5;
+  color: var(--gray-51);
+}
+
+.companies__item-label {
+  font-size: var(--text-small);
+  line-height: 1.4;
+  letter-spacing: 0.12px;
+  padding: 5px 22px 7px 22px;
+  background-color: var(--gray-247);
+  border-radius: var(--radius-chip);
 }
 </style>
